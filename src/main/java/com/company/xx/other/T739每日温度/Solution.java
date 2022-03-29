f1ecf84428a3e25cfec0edc222349511e9c476bc
21want28k <937930940@qq.com>
2022-03-29 16:51:38 +0800

    力扣

---
+++ src/main/java/com/company/xx/other/T739每日温度/Solution.java
@@ -0,0 +1,25 @@
+package com.company.xx.other.T739每日温度;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        if (n <= 1) {
+            return res;
+        }
+        // record index
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            int temperature = temperatures[i];
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperature) {
+                int preIndex = stack.pop();
+                res[preIndex] = i - preIndex;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
